0fc09f08a5595c567f83a0a37b0662b63fbe2d94
Gakuo <kairugakuo9@gmail.com>
2025-10-27 13:03:29 -0500

    feat: rest of files from previous commit change

--- backend/src/features/shared-workspace-dashboard/sharedWorkspaceController.js
+++ backend/src/features/shared-workspace-dashboard/sharedWorkspaceController.js
@@ -1,1 +1,21 @@
 //storage
+import { mockUsers, mockAssignments, mockWorkspaces } from "../../../../tests/utils/mockData.js";
+
+export function displayDashboard(userID) {
+
+    // 1st check - userID is valid (not null, undefined, a number, and greater than 0)
+    if (userID  == null ||userID == undefined || typeof userID !== "number" || userID <= 0) {
+        return "Error: Invalid user";
+    }
+
+    // 2nd check - lookup user in mock data -> evaluates to TRUE or FALSE
+    const userExists = Object.values(mockUsers).some(
+        (user) => user.id === userID
+    );
+    
+    if (userExists) {
+        return "Dashboard Loaded";
+    } else {
+        return "Error: User Not Found";
+    }
+}
