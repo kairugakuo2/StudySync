4d8e4e0af87128a72d389643503f87894c091ecc
KaiserBaam <130789515+KaiserBaam@users.noreply.github.com>
2025-12-08 02:39:18 -0600

    Update SessionManager.jsx

--- frontend/src/pages/sessionManager/SessionManager.jsx
+++ frontend/src/pages/sessionManager/SessionManager.jsx
@@ -1,9 +1,9 @@
-import sessions from "../../components/sessions.jsx";
+import sessionMan from "../../components/sessions.jsx";
 
 export default function sessionManager() {
   return (
     <div className="w-full h-full">
-      <sessions />
+      <sessionMan />
     </div>
   );
 }
